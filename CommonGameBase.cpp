12fe479db42e68e48e4bb589268bff160b1d917e
Taira Takahashi <taira.takahashi@dena.com>
2013-11-08 23:04:38 +0900

    メモリリーク修正。リファクタリング

--- CommonGameBase.cpp
+++ CommonGameBase.cpp
@@ -3,27 +3,28 @@
 
 namespace CommonGameBase
 {
-	using namespace cocos2d;
+    using namespace cocos2d;
     
-	/* IDConnector */
-	unsigned int IDConnector::id = 0;
+    /* IDConnector */
+    unsigned int IDConnector::id = 0;
 
-	/* ScreenBase */
-	ScreenBase::ScreenBase()
-	{
-		_scene = cocos2d::Scene::create();
-		_scene->addChild(this);
-	}
-	
-	ScreenBase::~ScreenBase()
-	{
-	}
-	
-	
-	SpriteBatchNode *ScreenBase::registerSprite(const char *filename)
-	{
-		SpriteBatchNode *batch = SpriteBatchNode::create(filename);
-		addChild(batch);
-		return batch;
-	}
+    /* ScreenBase */
+    ScreenBase::ScreenBase()
+    {
+        _scene = cocos2d::Scene::create();
+        _scene->retain();
+        _scene->addChild(this);
+    }
+
+    ScreenBase::~ScreenBase()
+    {
+        _scene->release();
+    }
+
+    SpriteBatchNode *ScreenBase::registerSprite(const char *filename)
+    {
+        SpriteBatchNode *batch = SpriteBatchNode::create(filename);
+        addChild(batch);
+        return batch;
+    }
 }
