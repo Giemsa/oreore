cddf8f58be2d0fabca6b14c74020435cc5bb52a3
Giemsa <mithrilworks@gmail.com>
2013-12-02 14:30:25 +0900

    不具合修正

--- cocos2d/Move.cpp
+++ cocos2d/Move.cpp
@@ -8,7 +8,7 @@ namespace cocos2d
 	Move *Move::create(const float duration, const Point &vec)
 	{
 		Move *action = new Move();
-		action->initWithDuration();
+		action->initWithDuration(duration, vec);
 		action->autorelease();
 		return action;
 	}
@@ -26,19 +26,23 @@ namespace cocos2d
 
 	void Move::update(float time)
 	{
-		getOriginalTarget()->setPosition(dpos + vec);
+		getOriginalTarget()->setPosition(getOriginalTarget()->getPosition() + vec);
 	}
 
 	void Move::startWithTarget(Node *target)
 	{
 		ActionInterval::startWithTarget(target);
-		dpos = target->getPosition();
 	}
 
 	void Move::stop(void)
 	{
-		getOriginalTarget()->setPosition(dpos);
 		ActionInterval::stop();
+	}
+
+	Move *Move::reverse() const
+	{
+        CCASSERT(0, "reverse is not implemented");
+		return null;
 	}
 
 	Move *Move::clone() const
--- cocos2d/Move.h
+++ cocos2d/Move.h
@@ -8,17 +8,18 @@ namespace cocos2d
 	class Move : public ActionInterval
 	{
 	private:
-		Point vec, dpos;
+		Point vec;
 	public:
 		Move() { }
 
-		static Move* create(const float duration, const cocos2d::Point &vec);
-		bool initWithDuration(const float duration, const cocos2d::Point &vec);
+		static Move* create(const float duration, const Point &vec);
+		bool initWithDuration(const float duration, const Point &vec);
 
 		virtual void startWithTarget(Node *pTarget);
 		virtual void update(float time);
 		virtual void stop(void) override;
 
+		virtual Move *reverse() const override;
 		virtual Move *clone() const override;
 	};
 }
