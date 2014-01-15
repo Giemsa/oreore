7e00179810923d9d7251ed4b4486454dcbe93792
Giemsa <mithrilworks@gmail.com>
2014-01-15 10:37:08 +0900

    AtrractMove追加

---
+++ oreore/cocos2d/actions/AttractMove.h
@@ -0,0 +1,31 @@
+#ifndef __OREORE_COCOS2D_ATTRACTMOVE_H__
+#define __OREORE_COCOS2D_ATTRACTMOVE_H__
+
+#include "cocos2d.h"
+
+namespace oreore
+{
+    class AttractMove : public cocos2d::ActionInterval
+    {
+    private:
+        cocos2d::Point dpos;
+        cocos2d::Point target;
+        cocos2d::Point accelVec, v0;
+
+    public:
+        AttractMove() { }
+        virtual ~AttractMove() { }
+
+        static AttractMove* create(const float duration, const cocos2d::Point &target, const cocos2d::Point &v0);
+        bool initWithDuration(const float duration, const cocos2d::Point &target, const cocos2d::Point &v0);
+
+        virtual void startWithTarget(cocos2d::Node *pTarget);
+        virtual void update(float time);
+        virtual void stop(void) override;
+
+        virtual AttractMove *reverse() const override;
+        virtual AttractMove *clone() const override;
+    };
+}
+
+#endif
