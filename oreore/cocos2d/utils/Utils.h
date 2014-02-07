00e8002f0472e3cc9194197b18cd89e1591537f4
Giemsa <mithrilworks@gmail.com>
2014-02-07 14:16:35 +0900

    update Utils for cocos2dx 3.0

--- oreore/cocos2d/utils/Utils.h
+++ oreore/cocos2d/utils/Utils.h
@@ -48,28 +48,28 @@ namespace oreore
         );
     }
 
-    inline cocos2d::CCPoint center()
+    inline cocos2d::Point center()
     {
-        const cocos2d::CCSize &s = cocos2d::CCDirector::sharedDirector()->getWinSize();
-        return cocos2d::CCPoint(s.width / 2, s.height / 2);
+        const cocos2d::Size &s = cocos2d::Director::getInstance()->getWinSize();
+        return cocos2d::Point(s.width / 2, s.height / 2);
     }
 
-    inline cocos2d::CCPoint center(cocos2d::CCNode *node)
+    inline cocos2d::Point center(cocos2d::Node *node)
     {
         const cocos2d::Size &s = node->getContentSize();
         return cocos2d::Point(s.width / 2, s.height / 2);
     }
 
-    inline cocos2d::CCPoint percent(const float x, const float y)
+    inline cocos2d::Point percent(const float x, const float y)
     {
-        const cocos2d::CCSize &s = cocos2d::CCDirector::sharedDirector()->getWinSize();
-        return cocos2d::CCPoint(s.width * x / 100, s.height * y / 100);
+        const cocos2d::Size &s = cocos2d::Director::getInstance()->getWinSize();
+        return cocos2d::Point(s.width * x / 100, s.height * y / 100);
     }
 
-    inline cocos2d::CCPoint percent(cocos2d::CCNode *node, const float x, const float y)
+    inline cocos2d::Point percent(cocos2d::Node *node, const float x, const float y)
     {
-        const cocos2d::CCSize &s = node->getContentSize();
-        return cocos2d::CCPoint(s.width * x / 100, s.height * y / 100);
+        const cocos2d::Size &s = node->getContentSize();
+        return cocos2d::Point(s.width * x / 100, s.height * y / 100);
     }
 
     inline int cut(const int v, const int n)
