f53c1c746061c523f76dc1ecfcfca88717ea70ad
Giemsa <mithrilworks@gmail.com>
2014-02-09 23:17:44 +0900

    fix MultiResolution
    Conflicts:
    	oreore/cocos2d/utils/MultiResolution.cpp
    	oreore/cocos2d/utils/MultiResolution.h

--- oreore/cocos2d/utils/MultiResolution.cpp
+++ oreore/cocos2d/utils/MultiResolution.cpp
@@ -5,22 +5,6 @@ namespace oreore
 {
     using namespace cocos2d;
 
-    /* Resolution */
-    Resolution::Resolution(const CCSize &size, const char *name) : size(size)
-    {
-        strncpy(directory, name, sizeof(directory));
-    }
-
-    bool Resolution::operator<(const Resolution &rhs) const
-    {
-        return size.width == rhs.size.width ? size.height < rhs.size.width : size.width < rhs.size.width;
-    }
-
-    bool Resolution::operator>(const Resolution &rhs) const
-    {
-        return size.width == rhs.size.width ? size.height > rhs.size.width : size.width > rhs.size.width;
-    }
-
     /* MultiResolution */
     const char *MultiResolution::names[ResolutionType::All] = {
         "resources-iphone",
@@ -33,26 +17,8 @@ namespace oreore
         "resources-medium",
         "resources-small"
     };
-    
-    enum
-    {
-        SEARCHPATH_IOS,
-        SEARCHPATH_ANDROID
-    };
-   
-    /*
-    const char *MultiResolution::searchPaths[2] = {
-        "Published-ios",
-        ""
-    };
-    */
-    
-    MultiResolution::MultiResolution()
-    {
 
-    }
-
-    cocos2d::CCSize MultiResolution::swap(const cocos2d::CCSize &size, const bool doSwap)
+    CCSize MultiResolution::swap(const CCSize &size, const bool doSwap)
     {
         if(doSwap)
             return CCSizeMake(size.height, size.width);
@@ -74,56 +40,27 @@ namespace oreore
         if(platform == kTargetIphone || platform == kTargetIpad)
         {
             if(size.height > 768)
-            {
-                rsize = CCSizeMake(2048, 1536);
                 order.push_back(names[ResolutionType::iPadHD]);
-            }
             else if(size.height > 640)
-            {
-                rsize = CCSizeMake(1536, 768);
                 order.push_back(names[ResolutionType::iPad]);
-            }
             else if(size.height > 480)
-            {
-                if(size.width < 1136)
-                    rsize = CCSizeMake(960, 640);
-                else
-                    rsize = CCSizeMake(1136, 640);
                 order.push_back(names[ResolutionType::iPhoneHD]);
-            }
             else
-            {
-                rsize = CCSizeMake(480, 320);
                 order.push_back(names[ResolutionType::iPhone]);
-            }
         }
         else if(platform == kTargetAndroid)
         {
             if(size.height > 1200)
-            {
-                rsize = CCSizeMake(1200, 800);
-                order.push_back(names[ResolutionType::XLarge]);
-            }
+                order.push_back(names[esolutionType::XLarge]);
             else if(size.height > 960)
-            {
-                rsize = CCSizeMake(960, 640);
                 order.push_back(names[ResolutionType::Large]);
-            }
             else if(size.height > 480)
-            {
-                rsize = CCSizeMake(720, 480);
                 order.push_back(names[ResolutionType::Medium]);
-            }
             else
-            {
-                rsize = CCSizeMake(568, 320);
                 order.push_back(names[ResolutionType::Small]);
-            }
         }
 
-        rsize = swap(rsize, v);
-        CCDirector::sharedDirector()->setContentScaleFactor(dsize.width / rsize.width);
-        eglView->setDesignResolutionSize(rsize.width, rsize.height,/* kResolutionFixedHeight */ kResolutionShowAll);
+        eglView->setDesignResolutionSize(dsize.width, dsize.height, kResolutionShowAll);
         CCFileUtils::sharedFileUtils()->setSearchPaths(searchPaths);
         CCFileUtils::sharedFileUtils()->setSearchResolutionsOrder(order);
     }
--- oreore/cocos2d/utils/MultiResolution.h
+++ oreore/cocos2d/utils/MultiResolution.h
@@ -6,16 +6,6 @@
 
 namespace oreore
 {
-	struct Resolution
-	{
-		cocos2d::CCSize size;
-		char directory[64];
-
-        Resolution(const cocos2d::CCSize &size, const char *name);
-        bool operator<(const Resolution &rhs) const;
-        bool operator>(const Resolution &rhs) const;
-	};
-
     namespace ResolutionType
     {
         enum Type
@@ -34,23 +24,25 @@ namespace oreore
         };
     }
 
-	class MultiResolution
-	{
+    class MultiResolution
+    {
         typedef std::vector<std::string> StringList;
-	private:
+    private:
         StringList searchPaths;
+        cocos2d::Size designSize;
         static const char *names[ResolutionType::All];
-        //static const char *searchPaths[2];
 
         cocos2d::CCSize swap(const cocos2d::CCSize &size, const bool doSwap);
-	public:
-		MultiResolution();
-		~MultiResolution() { }
+    public:
+        MultiResolution() : designSize() { }
+        MultiResolution(const cocos2d::CCSize &designSize) : designSize(designSize) { }
+        ~MultiResolution() { }
 
         void resolve();
         void addDirectory(const ResolutionType::Type res, const char *name);
         void addSearchPath(const char *name);
-	};
+        inline void setDesignSize(const cocos2d::CCSize &size) { designSize = size; }
+    };
 }
 
 #endif
