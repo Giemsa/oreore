42381e7b647d84f1666f98594a280fe399b1565b
Giemsa <mithrilworks@gmail.com>
2014-03-17 21:44:06 +0900

    MultiResolution修正
    Conflicts:
    	oreore/cocos2d/utils/MultiResolution.h

--- oreore/cocos2d/utils/MultiResolution.h
+++ oreore/cocos2d/utils/MultiResolution.h
@@ -32,6 +32,8 @@ namespace oreore
         inline ResolutionConfig(const cocos2d::Size &size, const std::string &dirname, const float scaleFactor, const int height)
             : size(size), dirname(dirname), scaleFactor(scaleFactor), height(height) { }
 
+        inline ResolutionConfig() : size(), dirname(""), scaleFactor(1.0f), height(0) { }
+
         inline void setConfig(const cocos2d::Size &size, const std::string &dirname, const float scaleFactor)
         {
             this->size = size;
@@ -48,6 +50,7 @@ namespace oreore
         ResolutionPolicy policy;
         static ResolutionList resolutions;
         float realScale, scaleH, scaleW;
+        ResolutionConfig currentConfig;
 
         cocos2d::Size swap(const cocos2d::Size &size, const bool doSwap);
         void initNames();
@@ -71,6 +74,7 @@ namespace oreore
         inline float getRealScaleFactor() const { return realScale; }
         inline float getScaleWidth() const { return scaleW; }
         inline float getScaleHeight() const { return scaleH; }
+        inline const ResolutionConfig &getCurrentResolutionConfig() const { return currentConfig; }
     };
 }
 
