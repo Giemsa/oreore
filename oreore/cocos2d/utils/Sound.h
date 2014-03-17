0c954c227f1628edebb38c9b2cc01f746d87725c
Giemsa <mithrilworks@gmail.com>
2014-03-17 13:54:12 +0900

    音周りの処理追加

---
+++ oreore/cocos2d/utils/Sound.h
@@ -0,0 +1,47 @@
+#ifndef __OREORE_COCOS2D_SOUND_H__
+#define __OREORE_COCOS2D_SOUND_H__
+
+#include "cocos2d.h"
+#include "SimpleAudioEngine.h"
+#include "../../null.h"
+
+namespace oreore
+{
+    class SoundManager : public cocos2d::CCNode
+    {
+    private:
+        float bgmVolume, seVolume;
+        std::string reservedMusicFile;
+
+        static std::auto_ptr<SoundManager> manager;
+        CREATE_FUNC(SoundManager);
+
+        void completeFading();
+    public:
+        static SoundManager &getInstance();
+
+        bool init(); // override
+
+        void playBGM(const std::string &filename, const float duration = 0.0f, const bool loop = true);
+        void stopBGM(const float duration = 0.0f);
+        unsigned int playSE(const std::string &filename);
+        void stopSE(const unsigned int id);
+
+        float getBGMVolume() const;
+        void setBGMVolume(const float volume);
+        float getSEVolume() const;
+        void setSEVolume(const float volume);
+
+        inline float getMaxBGMVolume() const { return bgmVolume; }
+        void setMaxBGMVolume(const float volume);
+        inline float getMaxSEVolume() const { return seVolume; }
+        void setMaxSEVolume(const float volume);
+
+        void fadeIn(const float duration);
+        void fadeOut(const float duration, const bool pauseOnComplete = false);
+        void fadeTo(const float volume, const float duration);
+        bool playWithFading(const std::string &filename, const float duration);
+    };
+}
+
+#endif
