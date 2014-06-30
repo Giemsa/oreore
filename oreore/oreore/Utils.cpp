b677acf7f3c72a5eb0839ac4d9773d19587bbfde
Giemsa <mithrilworks@gmail.com>
2014-06-30 20:56:01 +0900

    utils: support only C++11

--- oreore/oreore/Utils.cpp
+++ oreore/oreore/Utils.cpp
@@ -3,10 +3,28 @@
 
 namespace oreore
 {
+    namespace
+    {
+        Random &_getRandom()
+        {
+            static Random rnd(std::time(0));
+            return rnd;
+        }
+    }
+
     unsigned long random()
     {
-        static Random rnd(std::time(0));
-        return rnd.next();
+        return _getRandom().next();
+    }
+
+    unsigned long random(const unsigned long max)
+    {
+        return _getRandom().next(max);
+    }
+
+    unsigned long random(const unsigned long min, const unsigned long max)
+    {
+        return _getRandom().next(min, max);
     }
 }
 
--- oreore/oreore/Utils.h
+++ oreore/oreore/Utils.h
@@ -5,59 +5,91 @@
  * 雑多な便利ルーチン群
  */
 
-#if __cplusplus > 199711L
-#   define __IS_CPP_11__
-#   define __constexpr constexpr
-#else
-#   define __constexpr inline
-#endif
-
-#include <stdlib.h>
-#include <cstdarg>
 #include <cmath>
 #include <limits>
+#include <random>
 
 namespace oreore
 {
+    template<unsigned int N, typename T>
+    constexpr T rotl(const T v)
+    {
+        return (v << N) | (v >> (sizeof(T) * std::numeric_limits<char>::digits - N));
+    }
+
+    class xorshift
+    {
+    public:
+        using result_type = unsigned int;
+    private:
+        result_type x, y, z, w;
+
+        static constexpr result_type default_seed = 541238617;
+    public:
+        xorshift() : xorshift(default_seed) { }
+        explicit xorshift(const result_type seed)
+            : x(123456789), y(362436069), z(521288629), w(88675123)
+        {
+            x ^= seed;
+            y ^= rotl<8>(seed);
+            z ^= rotl<16>(seed);
+            w ^= rotl<24>(seed);
+        }
+
+        static constexpr result_type min() { return std::numeric_limits<result_type>::min(); }
+        static constexpr result_type max() { return std::numeric_limits<result_type>::max(); }
+        result_type operator()()
+        {
+            const result_type t = (x ^ (x << 11));
+            x = y;
+            y = z;
+            z = w;
+            return (w = (w ^ (w >> 19)) ^ (t ^ (t >> 8)));
+        }
+    };
+
     /*
      * Random Number Generator using Xorshift
      * You can use this class for std::random_shuffle()
      */
     class Random
     {
+        using result_type = unsigned long;
+        using dist_t = std::uniform_int_distribution<result_type>;
     private:
-        unsigned long x, y, z, w;
+        xorshift engine;
+        dist_t dist;
     public:
-        Random(const unsigned long seed) : x(123456789), y(362436069), z(seed), w(88675123)
+        Random(const result_type seed) : engine(seed)
+        { }
+
+        inline result_type next()
         {
-            w ^= seed;
-            x ^= seed;
+            return engine();
         }
 
-        unsigned long next()
+        inline result_type next(const result_type max)
         {
-            const unsigned long t = (x ^ (x << 11));
-            x = y;
-            y = z;
-            z = w;
-            return (w = (w ^ (w >> 19)) ^ (t ^ (t >> 8)));
+            return dist(engine, dist_t::param_type(0, max));
         }
 
-        inline unsigned long next(const unsigned long max) { return next() % max; }
-        inline unsigned long next(const unsigned long min, const unsigned long max) { return min + next(max - min); }
+        inline result_type next(const result_type min, const result_type max)
+        {
+            return dist(engine, dist_t::param_type(min, max));
+        }
 
-        inline unsigned long operator()(const unsigned long max) { return next(max); }
+        inline result_type operator()(const result_type max) { return next(max); }
     };
 
     unsigned long random();
-    inline unsigned long random(const unsigned long max) { return random() % max; }
-    inline unsigned long random(const unsigned long min, const unsigned long max) { return min + random(max - min); }
+    unsigned long random(const unsigned long max);
+    unsigned long random(const unsigned long min, const unsigned long max);
 
     template<typename T>
-    inline T random(const unsigned long max) { return static_cast<T>(random() % max); }
+    inline T random(const unsigned long max) { return static_cast<T>(random(max)); }
 
     template<typename T>
-    inline T random(const unsigned long min, const unsigned long max) { return static_cast<T>(min + random(max - min)); }
+    inline T random(const unsigned long min, const unsigned long max) { return static_cast<T>(random(min, max)); }
 
     /* 範囲指定する */
     template<typename T>
@@ -71,53 +103,29 @@ namespace oreore
     }
 
     template<typename T>
-    __constexpr T digits(const T n) { return static_cast<int>(std::ceil(log10(static_cast<long double>(n)))); }
+    constexpr T _digits(const T n)
+    {
+        return n > 0 ? _digits<T>(n / 10) + 1 : 0;
+    }
 
     template<typename T>
-    __constexpr int digits() { return std::numeric_limits<T>::digits10 + 1; }
+    constexpr T digits(const T n) { return _digits<T>(n / 10) + 1;  }
 
-    template<int N>
-    __constexpr int digits() { return digits(N); }
+    template<typename T>
+    constexpr int digits() { return std::numeric_limits<T>::digits10 + 1; }
 
-    __constexpr int sgn(const int v) { return ((v >> std::numeric_limits<int>::digits) << 1) + 1; }
-    __constexpr int sgn(const double v) { return (v > 0) - (v < 0); }
-    __constexpr int sgn(const float v) { return (v > 0) - (v < 0); }
+    constexpr int sgn(const int v) { return ((v >> std::numeric_limits<int>::digits) << 1) + 1; }
+    constexpr int sgn(const double v) { return (v > 0) - (v < 0); }
+    constexpr int sgn(const float v) { return (v > 0) - (v < 0); }
 
-    __constexpr double rad2deg(const double rad) { return rad * 180 / M_PI; }
+    constexpr double rad2deg(const double rad) { return rad * 180 / M_PI; }
 
-    __constexpr double deg2rad(const double deg) { return deg * M_PI / 180; }
+    constexpr double deg2rad(const double deg) { return deg * M_PI / 180; }
 }
 
 #ifndef va_copy
 #   define va_copy(dest, src) ((dest) = (src))
 #endif
-
-#define property_rw(Type, Name, Field)  \
-    private: \
-        Type Field;   \
-    public:     \
-        inline Type get ## Name() const { return Field; } \
-        inline void set ## Name(Type Field) { this->Field = Field; } \
-    private:
-
-#define property_r(Type, Name, Field)  \
-    private: \
-        Type Field;   \
-    public:     \
-        inline Type get ## Name() const { return Field; } \
-    private:
-
-#define property_w(Type, Name, Field)  \
-    private: \
-        Type Field;   \
-    public:     \
-        inline void set ## Name(Type Field) { this->Field = Field; } \
-    private:
-
-#define property(Attr, Type, Name, Field)  property_##Attr(Type, Name, Field)
-
-#undef __IS_CPP_11__
-#undef __constexpr
 
 #endif
 
